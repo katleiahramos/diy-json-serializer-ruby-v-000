5ba45ea5ead6f66786c150429d7d43ab3dee6ddb
Katleiah Ramos <katleiahramos@gmail.com>
2018-09-05 21:35:46 +0000

    automatically backed up by learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,6 @@
 class PostSerializer
   def self.serialize(post)
+<<<<<<< HEAD
 
     # start with the open brace to create a valid JSON object
     serialized_post = '{'
@@ -16,3 +17,21 @@ class PostSerializer
     serialized_post += '}'
   end
 end
+=======
+ 
+    # start with the open brace to create a valid JSON object
+    serialized_post = '{'
+ 
+    serialized_post += '"id": ' + post.id.to_s + ', '
+    serialized_post += '"title": "' + post.title + '", '
+    serialized_post += '"description": "' + post.description + '", '
+ 
+    # the author association can also be represented in JSON
+    serialized_post += '"author": {'
+    serialized_post += '"name": "' + post.author.name + '"}'
+ 
+    # and end with the close brace
+    serialized_post += '}'
+  end
+end
+>>>>>>> b4cd82858ec0b078a6eb43f7be7580d14d7f4238
